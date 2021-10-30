2481bc05af017e792afffcd7c06a7e4c62807c2c
Krzysztof Klimczyk <kklimczyk@atlassian.com>
2021-10-31 09:18:59 +1100

    move to OpenGL display and cmake

--- main.cpp
+++ main.cpp
@@ -1,13 +1,50 @@
-#include "tgaimage.h"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red   = TGAColor(255, 0,   0,   255);
+const char* WINDOW_TITLE = "Tiny Renderer";
+int WINDOW_WIDTH = 100;
+int WINDOW_HEIGHT = 100;
 
-int main(int argc, char** argv) {
-    TGAImage image(100, 100, TGAImage::RGB);
-    image.set(52, 41, red);
-    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-    image.write_tga_file("output.tga");
-    return 0;
+GLFWwindow* initOpenGLWindow() {
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return NULL;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    window = glfwCreateWindow(
+        WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        return NULL;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit())
+        return NULL;
+
+    return window;
 }
 
+int main(int argc, char** argv) {
+    GLFWwindow* window = initOpenGLWindow();
+    if (window == NULL)
+        return -1;
+
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+}
